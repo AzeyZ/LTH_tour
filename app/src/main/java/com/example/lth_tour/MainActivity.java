2db73c24010f7f28b76edab0a9eef8e9d54b2d57
Amatillah <gda10aal@student.lu.se>
2020-05-05 00:52:16 +0200

    Hemskärmen

--- app/src/main/java/com/example/lth_tour/MainActivity.java
+++ app/src/main/java/com/example/lth_tour/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.lth_tour;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,9 +21,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_LOCATION_PERMISSION= 1;
@@ -39,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView4);
-        button= (Button) findViewById(R.id.button);
+       // textView = (TextView) findViewById(R.id.textView4);
+        button= (Button) findViewById(R.id.searchButton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                                 REQUEST_CODE_LOCATION_PERMISSION
                         );
                     } else{
-                    getCurrentLocation();
+                    //getCurrentLocation();
                 }
             openPlatsActivity();
             }
@@ -62,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
+ /*   @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode==REQUEST_CODE_LOCATION_PERMISSION && grantResults.length>0){
@@ -70,14 +64,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
             }
         }
-    }
+    }*/
 
     public void openPlatsActivity(){
         Intent intent = new Intent(this, PlatsActivity.class);
         startActivity(intent);
     }
 
-    private void getCurrentLocation(){
+    /*private void getCurrentLocation(){
         LocationRequest locationRequest = new LocationRequest();
         locationRequest.setInterval(10000);
         locationRequest.setFastestInterval(3000);
@@ -101,6 +95,6 @@ public class MainActivity extends AppCompatActivity {
                 }, Looper.getMainLooper());
 
 
-    }
+    }*/
 }
 
