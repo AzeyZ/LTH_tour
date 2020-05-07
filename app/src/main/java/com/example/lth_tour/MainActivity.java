bfd06086c9951222987eb81f99d22c62cd8b20cd
AzeyZ <tna15aer@student.lu.se>
2020-05-07 19:27:18 +0200

    Rest of the changes needed to be pushed

--- app/src/main/java/com/example/lth_tour/MainActivity.java
+++ app/src/main/java/com/example/lth_tour/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.lth_tour;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.Looper;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,42 +26,73 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.material.snackbar.Snackbar;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_LOCATION_PERMISSION= 1;
+    // A reference to the service used to get location updates.
+    private GpsService mService = null;
+    // Tracks the bound state of the service.
+    private boolean mBound = false;
     private TextView textView;
     private Button button;
     private ImageView info_but;
     public double latitude;
     public double longitude;
+    private MyReceiver myReceiver;
+
+    // Monitors the state of the connection to the service.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            GpsService.LocalBinder binder = (GpsService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        myReceiver = new MyReceiver();
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
         textView = (TextView) findViewById(R.id.textView);
         button= (Button) findViewById(R.id.searchButton);
         info_but = (ImageView) findViewById(R.id.infoButton);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               startGPS(v);
+                //startGPS(v);
             }
         });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startGPS(v);
+                mService.requestLocationUpdates();
                 if(Utils.calculateDistanceTo(latitude,longitude,55.6971392, 13.1967143)[0] < 15) {
-                    openPlatsActivity();
+                    //openPlatsActivity();
+                    openGpsActivity();
                 }else{
                     openGpsActivity();
                 }
@@ -59,14 +101,60 @@ public class MainActivity extends AppCompatActivity {
         info_but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openPlatsActivity();;
+                openPlatsActivity();
             }
         });
-
-
-
+        // Bind to the service. If the service is in foreground mode, this signals to the service
+        // that since this activity is in the foreground, the service can exit foreground mode.
+        bindService(new Intent(this, GpsService.class), mServiceConnection,
+                Context.BIND_AUTO_CREATE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver,
+                new IntentFilter(GpsService.ACTION_BROADCAST));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mBound) {
+            // Unbind from the service. This signals to the service that this activity is no longer
+            // in the foreground, and the service can respond by promoting itself to a foreground
+            // service.
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+        super.onStop();
+    }
+
+    /**
+     * Returns the current state of the permissions needed.
+     */
+    private boolean checkPermissions() {
+        return  PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    private void requestPermissions() {
+        if(ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION
+            );
+        }
+    }
+
+    /*
     private void startGPS(View v){
         if(ContextCompat.checkSelfPermission(
                 getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
@@ -79,9 +167,7 @@ public class MainActivity extends AppCompatActivity {
             getCurrentLocation();
         }
     }
-
-
-
+    */
    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -100,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GpsActivity.class);
         startActivity(intent);
     }
-
+    /*
     private void getCurrentLocation(){
         LocationRequest locationRequest = new LocationRequest();
         locationRequest.setInterval(10000);
@@ -125,6 +211,20 @@ public class MainActivity extends AppCompatActivity {
                 }, Looper.getMainLooper());
 
 
+    }
+*/
+    /**
+     * Receiver for broadcasts sent by {@link GpsService}.
+     */
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Location location = intent.getParcelableExtra(GpsService.EXTRA_LOCATION);
+            if (location != null) {
+                Toast.makeText(MainActivity.this, Utils.getLocationText(location),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
 
